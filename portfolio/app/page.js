fb21c23e69b06e5ce9a72c5334449e62e1f2218e
naolumer <naolumer3@gmail.com>
2025-05-03 14:52:41 +0300

    contact form

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import About from "@/components/About";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
+import Contact from "@/components/Contact";
 
 
 
@@ -15,6 +16,7 @@ export default function Home() {
       <About/>
       <Services/>
       <Work/>
+      <Contact/>
     </>
   );
 }
