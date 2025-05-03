6013a32c596c3ab5bc3ff6c98a5c6244304e6487
naolumer <naolumer3@gmail.com>
2025-05-03 12:06:49 +0300

    changed bg properties of navbar onscroll

--- portfolio/components/Navbar.jsx
+++ portfolio/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import { assets } from '@/assets/assets'
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 
 const Navbar = () => {
+
+    const [isScroll, setIsScroll] = useState(false)
 
     const sideMenuRef = useRef();
     const openMenu = ()=>{
@@ -11,16 +13,27 @@ const Navbar = () => {
     const closeMenu = ()=>{
         sideMenuRef.current.style.transform = "translateX(16rem)"
     }
+
+    useEffect(()=>{
+        window.addEventListener("scroll", ()=>{
+            if(scrollY > 50){
+                setIsScroll(true)
+            }else{
+                {setIsScroll(false)}
+            }
+        })
+    },[])
+
   return (
     <>
-     <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4
-     flex items-center justify-between z-50 bg-[#fcf4ff] opacity-95'>
+     <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4
+     flex items-center justify-between z-50 ${isScroll ? "bg-white/50 backdrop-blur-lg shadow-sm": ""} `}>
         <a href="#top">
             <Image src={assets.logo} className="w-28 cursor-pointer mr-14" alt=''></Image>
         </a>
 
-        <ul className='hidden md:flex items-center gap-6 lg-gap-8
-        rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
+        <ul className={`hidden md:flex items-center gap-6 lg-gap-8
+        rounded-full px-12 py-3 ${isScroll ? "":"bg-white shadow-sm bg-opacity-50"} `}>
             <li><a className='font-ovo' href="#top">Home</a></li>
             <li><a className='font-ovo' href="#about">About me</a></li>
             <li><a className='font-ovo' href="#services">Services</a></li>
