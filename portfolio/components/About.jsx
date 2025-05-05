7a35f7199aad167db17bab54f6c1d97ccee31abd
naolumer <naolumer3@gmail.com>
2025-05-05 15:27:24 +0300

    added animation for header section

--- portfolio/components/About.jsx
+++ portfolio/components/About.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const About = ({isDarkMode, setIsDarkMode}) => {
   return (
-    <div id='about' className='w-full px-[12%] py-10 scroll-mt-20 '>
+    <motiondiv id='about' className='w-full px-[12%] py-10 scroll-mt-20 '>
         <h4 className='text-center mb-2 text-lg font-Ovo'>Introduction</h4>
         <h2 className='text-center text-5xl font-Ovo'>About me</h2>
 
