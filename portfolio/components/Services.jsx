7a8bc3f7b141d42da4ec692cd34c10df8fbe747c
naolumer <naolumer3@gmail.com>
2025-05-05 16:15:30 +0300

    added animation for services section

--- portfolio/components/Services.jsx
+++ portfolio/components/Services.jsx
@@ -2,25 +2,47 @@ import React from 'react';
 import { serviceData } from '@/assets/assets';
 import { assets } from '@/assets/assets';
 import Image from 'next/image';
+import {motion} from "motion/react"
 
 const Services = ({isDarkMode, setIsDarkMode}) => {
   return (
-    <div id="services" className='w-full px-[12%] py-10 scroll-mt-20'>
-      <h4 className='text-center mb-2 text-lg font-Ovo'>
+    <motion.div 
+    initial = {{opacity: 0}}
+    whileInView = {{opacity:1}}
+    transition={{duration: 1}}
+    id="services" className='w-full px-[12%] py-10 scroll-mt-20'>
+      <motion.h4 
+      initial = {{y: -20, opacity: 0}}
+      whileInView = {{y:0, opacity:1}}
+      transition={{duration: 0.5, delay: 0.3}}
+      className='text-center mb-2 text-lg font-Ovo'>
         What I offer
-      </h4>
-      <h2 className='text-center text-5xl font-Ovo'>
+      </motion.h4>
+      <motion.h2 
+      intial = {{opacity: 0,y: -20}}
+      whileInView = {{y:0, opacity:1}}
+      transition={{duration: 0.5, delay:0.5}}
+      className='text-center text-5xl font-Ovo'>
         My Services
-      </h2>
+      </motion.h2>
 
-      <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+      <motion.p 
+      initial = {{opacity: 0}}
+      whileInView = {{opacity:1}}
+      transition={{duration: 0.5, delay:0.7}}
+      className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
         I am a frontend developer from California, USA with 10 years of experience
         in multiple companies like Microsoft, Tesla and Apple.
-      </p>
+      </motion.p>
 
-      <div className='grid  [grid-template-columns:repeat(auto-fit,_minmax(200px,_1fr))] gap-6 my-10'>
+      <motion.div 
+      initial = {{opacity: 0}}
+      whileInView = {{opacity:1}}
+      transition={{duration: 0.6, delay:0.9}}
+      className='grid  [grid-template-columns:repeat(auto-fit,_minmax(200px,_1fr))] gap-6 my-10'>
         {serviceData.map(({ icon, title, description, link }, index) => (
-            <div
+            <motion.div
+            whileHover = {{scale: 1.05}}
             key={index}
             className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-[4px_4px_0_#000] 
             cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500 dark:hover:bg-[#11003F] 
@@ -34,10 +56,10 @@ const Services = ({isDarkMode, setIsDarkMode}) => {
             <a href={link} className='flex items-center gap-2 text-sm mt-5'>
                 Read more <Image alt='' src={assets.right_arrow} className='w-4' />
             </a>
-            </div>
+            </motion.div>
         ))}
-        </div>
-    </div>
+        </motion.div>
+    </motion.div>
   );
 };
 
