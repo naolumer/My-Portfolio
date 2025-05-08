2091f7e2e88c4938d10d21efc03aff12b4fd472d
naolumer <naolumer3@gmail.com>
2025-05-08 11:17:59 +0300

    changed my profile picture

--- portfolio/components/Header.jsx
+++ portfolio/components/Header.jsx
@@ -12,8 +12,8 @@ const Header = ({isDarkMode,setIsDarkMode}) => {
         whileInView={{scale: 1}}
         transition={{duration: 0.8, type:"spring", stiffness:100}}
         >
-            <Image src={assets.profile_img} alt=''
-            className='rounded-full w-32'/>
+            <Image src={assets.profile_pic} alt=''
+            className='rounded-full w-36 h-36'/>
         </motion.div>
             <motion.h3 
                 initial = {{y:-20, opacity: 0}}
